ef375df5d8fa3dd42bb32c2248501f6fc98e469a
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-02 23:16:29 +0300

    feat: add ItemAttachment entity

---
+++ src/LostAndFound.Core/Entities/ItemAttachment.cs
@@ -0,0 +1,9 @@
+﻿namespace LostAndFound.Core.Entities;
+
+public class ItemAttachment
+{
+    public int Id { get; set; }
+    public string FilePath { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
