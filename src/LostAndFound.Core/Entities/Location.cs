685c6b2aff14add2ae991a520628551ce31dd52b
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-02 15:46:10 +0300

    feat: Add Location entity with validation and relationships

---
+++ src/LostAndFound.Core/Entities/Location.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LostAndFound.Core.Entities;
+
+public class Location
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string LocationName { get; set; } = string.Empty;  
+    public LocationType LocationType { get; set; }
+
+    // Foreign key 
+    public int DepartmentId { get; set; }
+
+    // Navigation property
+    public Department Department { get; set; } = default!;
+}
