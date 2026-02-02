c558ce4b72a420f04e0a56ab1a2a8eb087d240ac
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-02 15:21:36 +0300

    feat: Add Department entity with validation and relationships.

---
+++ src/LostAndFound.Core/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LostAndFound.Core.Entities;
+
+public class Department
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string DepartmentName { get; set; } = string.Empty;
+
+    // Foreign key to University
+    public int UniversityId { get; set; }
+
+    // Navigation property
+    public University University { get; set; } = default!;
+    public ICollection<Location> Locations { get; set; } = new List<Location>();
+}
