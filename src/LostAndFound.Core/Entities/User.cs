3a0506529d7642de6611189a807c255264814584
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-02 23:17:11 +0300

    feat: add User entity

---
+++ src/LostAndFound.Core/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿namespace LostAndFound.Core.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public DateTime LastLoginAt { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    // Navigation properties
+    public UserRole Role { get; }
+    public ICollection<Report> Reports { get; set; } = new List<Report>();
+    public ICollection<Claim> Claims { get; set; } = new List<Claim>();
+    public ICollection<Handover> HandledBy { get; set; } = new List<Handover>();
+    public ICollection<Handover> ReciverUser { get; set; } = new List<Handover>();
+    public ICollection<Match> Matches { get; set; } = new List<Match>();
+    public ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+}
