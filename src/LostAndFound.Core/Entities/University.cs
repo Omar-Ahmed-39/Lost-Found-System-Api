9620a2ec4a491c096a6ff713b5d7f92d54b098d8
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-02 15:21:08 +0300

    feat: Add University entity with validation and navigation.

---
+++ src/LostAndFound.Core/Entities/University.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LostAndFound.Core.Entities;
+
+public class University
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string UniversityName { get; set; } = string.Empty;
+
+    // Navigation property 
+    public ICollection<Department> Departments { get; set; } = new List<Department>();
+
+}
