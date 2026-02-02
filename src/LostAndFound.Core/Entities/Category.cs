61a4f92e92aeb7de5d866567c7c27541843f732e
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-02 23:16:04 +0300

    feat: add Category entity

---
+++ src/LostAndFound.Core/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace LostAndFound.Core.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    //Foreign key
+    public int ReportId { get; set; }
+
+    //Navigation properties
+    public ItemReport Report { get; set; } = default!;
+
+}
