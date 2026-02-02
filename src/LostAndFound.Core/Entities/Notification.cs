02ffeac21a3cd684718b08c1f2c2e366110e507c
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-02 23:16:59 +0300

    feat: add Notification entity

---
+++ src/LostAndFound.Core/Entities/Notification.cs
@@ -0,0 +1,15 @@
+﻿namespace LostAndFound.Core.Entities;
+
+public class Notification
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public DateTime SentAt { get; set; }
+
+    // Foreign key
+    public int UserId { get; set; }
+
+    // Navigation property
+    public User User { get; set; } = default!;
+}
