bb6220889da762946149de451bb5e2586a5195f9
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-13 00:17:50 +0300

    fix: Refactor Category-ItemReport relationship to collection.

--- src/LostAndFound.Core/Entities/Category.cs
+++ src/LostAndFound.Core/Entities/Category.cs
@@ -5,10 +5,7 @@ public class Category
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 
-    //Foreign key
-    public int ReportId { get; set; }
-
     //Navigation properties
-    public ItemReport Report { get; set; } = default!;
+    public ICollection<ItemReport> Reports { get; set; } = new List<ItemReport>();
 
 }
