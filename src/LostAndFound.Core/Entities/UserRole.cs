045d72f363efd47c42230ca7e9c65201c821a002
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-14 13:55:22 +0300

    feat: add nav prop with User and Role

--- src/LostAndFound.Core/Entities/UserRole.cs
+++ src/LostAndFound.Core/Entities/UserRole.cs
@@ -3,5 +3,9 @@
 public class UserRole
 {
     public int UserId { get; set; }
-    public int RoleId { get; set; } 
+    public int RoleId { get; set; }
+
+    // Navigation properties
+    public User User { get; set; } = default!;
+    public Role Role { get; set; } = default!;
 }
