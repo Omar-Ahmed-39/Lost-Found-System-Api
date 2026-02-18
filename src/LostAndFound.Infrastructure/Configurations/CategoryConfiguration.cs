4387d94cc6876f86ba40d8c68349ff041f9ee512
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-19 01:06:15 +0300

    Add EF Core packages and Category entity configuration

---
+++ src/LostAndFound.Infrastructure/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using LostAndFound.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LostAndFound.Infrastructure.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name).IsRequired().HasMaxLength(100);
+    }
+}
