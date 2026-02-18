40fe2a645fdb142e099a25c1ccd1088b4923b258
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-19 01:07:29 +0300

    feat: add ClaimConfigration class

--- src/LostAndFound.Infrastructure/Configurations/CategoryConfiguration.cs
+++ src/LostAndFound.Infrastructure/Configurations/CategoryConfiguration.cs
@@ -9,7 +9,9 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
     public void Configure(EntityTypeBuilder<Category> builder)
     {
         builder.ToTable("Categories");
+
         builder.HasKey(c => c.Id);
+
         builder.Property(c => c.Name).IsRequired().HasMaxLength(100);
     }
 }
---
+++ src/LostAndFound.Infrastructure/Configurations/ClaimConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using LostAndFound.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LostAndFound.Infrastructure.Configurations;
+
+public class ClaimConfiguration : IEntityTypeConfiguration<Claim>
+{
+    public void Configure(EntityTypeBuilder<Claim> builder)
+    {
+        builder.ToTable("Claims");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(a => a.ApprovalStatus)
+            .HasConversion<int>();
+
+        builder.Property(a => a.Remarks)
+            .HasMaxLength(300);
+
+        builder.HasOne(x => x.Report)
+            .WithMany(c => c.Claims)
+            .HasForeignKey(x => x.ReportId);
+    }
+}
