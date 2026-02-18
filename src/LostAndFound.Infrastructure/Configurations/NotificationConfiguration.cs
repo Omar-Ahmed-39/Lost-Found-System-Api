e0e67aa26cb42386486e69cc0bee7f6042c81148
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-19 01:13:02 +0300

    feat; add Notification configration class

---
+++ src/LostAndFound.Infrastructure/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using LostAndFound.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LostAndFound.Infrastructure.Configurations;
+
+public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.ToTable("Notifications");
+
+        builder.HasKey(n => n.Id);
+
+        builder.Property(n => n.Title)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(n => n.Message)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(n => n.SentAt)
+            .IsRequired();
+
+        // Relationship 
+
+        // Notification -> User (Many-to-One)
+        builder.HasOne(n => n.User)
+            .WithMany(u => u.Notifications)
+            .HasForeignKey(n => n.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
