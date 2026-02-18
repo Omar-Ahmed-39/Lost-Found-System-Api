b279bcfde05fc0b58fcf361f6476cb22d803c509
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-19 01:14:56 +0300

    feat: add University configuration class

---
+++ src/LostAndFound.Infrastructure/Configurations/UniversityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using LostAndFound.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LostAndFound.Infrastructure.Configurations;
+
+public class UniversityConfiguration : IEntityTypeConfiguration<University>
+{
+    public void Configure(EntityTypeBuilder<University> builder)
+    {
+        builder.ToTable("Universities");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        // Relationships
+
+        // University -> Departments (One-to-Many)
+        builder.HasMany(u => u.Departments)
+            .WithOne(d => d.University)
+            .HasForeignKey(d => d.UniversityId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
