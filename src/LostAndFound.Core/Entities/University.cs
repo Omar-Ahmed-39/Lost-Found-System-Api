cee4a131b11d3096e6f3b3c0e79e0692f39fecfd
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-19 01:02:27 +0300

    fix: Renamed entity properties for consistency (Name)

--- src/LostAndFound.Core/Entities/University.cs
+++ src/LostAndFound.Core/Entities/University.cs
@@ -5,7 +5,7 @@ namespace LostAndFound.Core.Entities;
 public class University
 {
     public int Id { get; set; }
-    public string UniversityName { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
     // Navigation property 
     public ICollection<Department> Departments { get; set; } = new List<Department>();
