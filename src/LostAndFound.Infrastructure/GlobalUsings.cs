c86991401bce1a30fe17de3e6392dd9a153079bc
Ali Bin-Samida <a711879040@gmail.com>
2026-02-27 02:16:26 +0300

    feat: Add missing global using directive for LostAndFound.Core.Interfaces in GlobalUsings.cs

--- src/LostAndFound.Infrastructure/GlobalUsings.cs
+++ src/LostAndFound.Infrastructure/GlobalUsings.cs
@@ -1,3 +1,4 @@
 ﻿global using LostAndFound.Core.Entities;
+global using LostAndFound.Core.Interfaces;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
