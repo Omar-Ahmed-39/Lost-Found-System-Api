5aa431061b3977c6eac1ec9e2384b8a5910ed571
Ali Bin-Samida <a711879040@gmail.com>
2026-02-27 02:16:34 +0300

    feat: Update GetPagedAsync method to return a tuple with items and total count

--- src/LostAndFound.Core/Interfaces/IGenericRepository.cs
+++ src/LostAndFound.Core/Interfaces/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace LostAndFound.Core.Interfaces;
 public interface IGenericRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, bool isTracking = false, params Expression<Func<T, object>>[]? includes);
-    Task<IEnumerable<T>> GetPagedAsync(int pageNumber, int pageSize, Expression<Func<T, bool>>? predicate = null, bool isTracking = false, params Expression<Func<T, object>>[]? includes);
+    Task<(IEnumerable<T> Items, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize, Expression<Func<T, bool>>? predicate = null, bool isTracking = false, params Expression<Func<T, object>>[]? includes);
     Task<T?> GetAsync(Expression<Func<T, bool>> predicate, bool isTracking = false, params Expression<Func<T, object>>[]? includes);
     Task<T?> FindAsync(object id);
     Task AddAsync(T entity);
