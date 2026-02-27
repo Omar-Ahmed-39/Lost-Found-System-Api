0094d228fddf1e04a99214c8f169957b3d173e74
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-28 01:40:56 +0300

    feat: add claim interface

---
+++ src/LostAndFound.Core/Interfaces/IClaimRepository.cs
@@ -0,0 +1,10 @@
+﻿using LostAndFound.Core.Entities;
+
+namespace LostAndFound.Core.Interfaces;
+
+public interface IClaimRepository : IGenericRepository<Claim>
+{
+    Task<bool> ApproveClaimAsync(int claimId, int adminId);
+    Task<bool> RejectClaimAsync(int claimId, int adminId, string remarks);
+    Task<bool> CancelClaimAsync(int claimId, int userId);
+}
