230901f0699f7eab0a5fc7ff4713cfb4b286fa16
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-28 01:40:25 +0300

    feat: add ItemReport Interface

--- src/LostAndFound.Core/Interfaces/IItemReportRepository.cs
+++ src/LostAndFound.Core/Interfaces/IItemReportRepository.cs
@@ -4,4 +4,7 @@ namespace LostAndFound.Core.Interfaces;
 
 public interface IItemReportRepository : IGenericRepository<ItemReport>
 {
+    Task<bool> UpdateReportAsync(ItemReport report, int userId, bool isAdmin);
+    Task<bool> DeleteReportAsync(int reportId, int userId, bool isAdmin);
+    Task<bool> CloseReportAsync(int reportId);
 }
