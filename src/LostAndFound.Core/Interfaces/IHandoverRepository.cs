84bfb05bb37ab351e75bf6f820df49705a192681
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-02-28 01:40:43 +0300

    feat: add handover interface

---
+++ src/LostAndFound.Core/Interfaces/IHandoverRepository.cs
@@ -0,0 +1,8 @@
+﻿using LostAndFound.Core.Entities;
+
+namespace LostAndFound.Core.Interfaces;
+
+public interface IHandoverRepository : IGenericRepository<Handover>
+{
+    Task<bool> CompleteHandoverAsync(int claimId, Handover handoverData, int adminId);
+}
