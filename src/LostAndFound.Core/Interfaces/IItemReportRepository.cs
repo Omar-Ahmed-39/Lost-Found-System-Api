c671ed34b607b5e6bec6b4bf3d655f51ec5a081d
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-03-06 02:14:42 +0300

    feat: add ItemReportRepository interface

--- src/LostAndFound.Core/Interfaces/IItemReportRepository.cs
+++ src/LostAndFound.Core/Interfaces/IItemReportRepository.cs
@@ -1,10 +1,15 @@
 using LostAndFound.Core.Entities;
+using LostAndFound.Core.Filters;
 
 namespace LostAndFound.Core.Interfaces;
 
 public interface IItemReportRepository : IGenericRepository<ItemReport>
 {
-    Task<bool> UpdateReportAsync(ItemReport report, int userId, bool isAdmin);
-    Task<bool> DeleteReportAsync(int reportId, int userId, bool isAdmin);
-    Task<bool> CloseReportAsync(int reportId);
+    Task<ItemReport> CreateAsync(ItemReport report);
+    Task<ItemReport> UpdateAsync(ItemReport Report, int userId, bool isAdmin);
+    Task<ItemReport> DeleteAsync(int Report, int userId, bool isAdmin);
+    Task<ItemReport> CancelAsync(int Report, int userId, bool isAdmin);
+    Task<ItemReport?> GetByIdasync(int reportId);
+    Task<IEnumerable<ItemReport>> GetItemReportsAsync();
+    Task<IEnumerable<ItemReport>> FilterAsync(ItemReportFilter filter);
 }
