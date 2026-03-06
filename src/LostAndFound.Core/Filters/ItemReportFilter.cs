31c1cc772556ce6842dcfa200de5ecf6490f16a9
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-03-06 03:17:28 +0300

    refact: the prop name

--- src/LostAndFound.Core/Filters/ItemReportFilter.cs
+++ src/LostAndFound.Core/Filters/ItemReportFilter.cs
@@ -5,7 +5,7 @@ public class ItemReportFilter
     public int? CategoryId { get; set; }
     public enStatusType? StatusType { get; set; }
     public enReportType? ReportType { get; set; }
-    public DateTime? DateFrom { get; set; }
+    public DateTime? FromDate { get; set; }
     public DateTime? ToDate { get; set; }
     public String? Search { get; set; }
 
