6d0f26b2cc5f49b95cb5c81d6a3caca3390b7e86
Ali Bin-Samida <a711879040@gmail.com>
2026-03-06 03:25:17 +0300

    feat: implement JwtProvider for token generation and management

---
+++ src/LostAndFound.Infrastructure/Repository/JwtProvider.cs
@@ -0,0 +1,48 @@
+using System.Security.Claims;
+using LostAndFound.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Claim = System.Security.Claims.Claim;
+
+namespace LostAndFound.Infrastructure.Repository;
+
+public class JwtProvider : IJwtProvider
+{
+    private readonly IConfiguration _configuration;
+    public JwtProvider(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public string GenerateToken(User user, IList<string> roles)
+    {
+        var claims = new List<System.Security.Claims.Claim>
+        {
+            new System.Security.Claims.Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new System.Security.Claims.Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new System.Security.Claims.Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) ,
+            new System.Security.Claims.Claim("Name",user.Name ?? string.Empty)
+        };
+
+        if (roles != null)
+        {
+            foreach (var role in roles)
+            {
+                claims.Add(new System.Security.Claims.Claim(ClaimTypes.Role, role));
+            }
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtOptions:SecretKey"] ?? string.Empty));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JwtOptions:Issuer"],
+            audience: _configuration["JwtOptions:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: creds
+        );
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
