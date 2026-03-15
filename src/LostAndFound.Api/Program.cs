f84a18bc1e54e36a1895b37117ef382edd6e0621
Ali Bin-Samida <a711879040@gmail.com>
2026-03-15 04:20:02 +0300

    feat: Add Swagger documentation setup in Program.cs

--- src/LostAndFound.Api/Program.cs
+++ src/LostAndFound.Api/Program.cs
@@ -2,12 +2,14 @@ using LostAndFound.Core;
 using LostAndFound.Infrastructure;
 using LostAndFound.Infrastructure.Interceptors;
 using Microsoft.EntityFrameworkCore;
+using LostAndFound.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerDocumentation();
 
 builder.Services.AddSingleton<AuditInterceptor>();
 
@@ -23,32 +25,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lost and Found API v1"));
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
