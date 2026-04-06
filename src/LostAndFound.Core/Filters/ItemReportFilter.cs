4b9dc8979eb28f2ef952cb4c2d7533f85069460a
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-04-06 23:53:46 +0300

    feat: add some prop to match the dashboard

--- src/LostAndFound.Core/Filters/ItemReportFilter.cs
+++ src/LostAndFound.Core/Filters/ItemReportFilter.cs
@@ -2,11 +2,11 @@
 
 public class ItemReportFilter
 {
+    public string? Search { get; set; }
     public int? CategoryId { get; set; }
+    public int? LocationId { get; set; }
     public enStatusType? StatusType { get; set; }
     public enReportType? ReportType { get; set; }
     public DateTime? FromDate { get; set; }
     public DateTime? ToDate { get; set; }
-    public String? Search { get; set; }
-
 }
