dc8074e28a24b949ad88f69d2ec83e7945e8c2f0
Ali Bin-Samida <a711879040@gmail.com>
2026-04-11 17:43:33 +0300

    feat(feedback): add FeedbackConfiguration for entity configuration

---
+++ src/LostAndFound.Infrastructure/Configurations/FeedbackConfiguration.cs
@@ -0,0 +1,24 @@
+namespace LostAndFound.Infrastructure.Configurations;
+
+public class FeedbackConfiguration : IEntityTypeConfiguration<Feedback>
+{
+    public void Configure(EntityTypeBuilder<Feedback> builder)
+    {
+        builder.ToTable("Feedbacks");
+
+        builder.HasKey(f => f.Id);
+        
+        builder.Property(f => f.Subject)
+               .HasMaxLength(150)
+               .IsRequired();
+               
+        builder.Property(f => f.Message)
+               .HasMaxLength(1000)
+               .IsRequired();
+
+        builder.HasOne(f => f.User)
+               .WithMany() 
+               .HasForeignKey(f => f.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
