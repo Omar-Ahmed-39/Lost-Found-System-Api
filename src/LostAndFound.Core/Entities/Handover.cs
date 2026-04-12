65f6deb78703ba6cf8996ec3f621d4fff351b89a
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-04-12 19:08:47 +0300

    refac: improve by incloud CreatedAt and UpdateAt and change the name of the prop to match the key

--- src/LostAndFound.Core/Entities/Handover.cs
+++ src/LostAndFound.Core/Entities/Handover.cs
@@ -6,19 +6,18 @@ public class Handover
     public enIdType IdType { get; set; }
     public string IdNumber { get; set; } = string.Empty;
     public string ImagePath { get; set; } = string.Empty;
-    public DateTime HandoverDate { get; set; }
+    public DateTime HandoverDate { get; set; } = DateTime.UtcNow;
     public string Notes { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
-    // Foreign key
     public int LocationId { get; set; }
     public int ReceiverUserId { get; set; }
     public int HandedByUserId { get; set; }
     public int ClaimId { get; set; }
 
-    // Navigation properties
     public Location Location { get; set; } = default!;
     public User ReceiverUser { get; set; } = default!;
-    public User GivenByUser { get; set; } = default!;
+    public User HandedByUser { get; set; } = default!;
     public Claim Claim { get; set; } = default!;
-
 }
