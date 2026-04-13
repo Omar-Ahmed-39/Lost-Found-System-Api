28881f5aad4851978b1577f13106079958ecaff1
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-04-13 08:36:42 +0300

    feat: Add IHandoverService interface with CreateHandoverAsync method

---
+++ src/LostAndFound.Core/Interfaces/Services/IHandoverService.cs
@@ -0,0 +1,8 @@
+﻿using LostAndFound.Core.Entities;
+
+namespace LostAndFound.Core.Interfaces;
+
+public interface IHandoverService
+{
+    Task<bool> CreateHandoverAsync(Handover handover, int adminUserId);
+}
