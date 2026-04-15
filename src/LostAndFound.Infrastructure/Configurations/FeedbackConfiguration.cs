6e0ad89e37278772b806e89ce1370d4d87063b4f
ALI <a711879040@gmail.com>
2026-04-15 13:47:43 +0300

    fix: correct User navigation property in FeedbackConfiguration

--- src/LostAndFound.Infrastructure/Configurations/FeedbackConfiguration.cs
+++ src/LostAndFound.Infrastructure/Configurations/FeedbackConfiguration.cs
@@ -2,23 +2,23 @@ namespace LostAndFound.Infrastructure.Configurations;
 
 public class FeedbackConfiguration : IEntityTypeConfiguration<Feedback>
 {
-    public void Configure(EntityTypeBuilder<Feedback> builder)
-    {
-        builder.ToTable("Feedbacks");
+       public void Configure(EntityTypeBuilder<Feedback> builder)
+       {
+              builder.ToTable("Feedbacks");
 
-        builder.HasKey(f => f.Id);
-        
-        builder.Property(f => f.Subject)
-               .HasMaxLength(150)
-               .IsRequired();
-               
-        builder.Property(f => f.Message)
-               .HasMaxLength(1000)
-               .IsRequired();
+              builder.HasKey(f => f.Id);
 
-        builder.HasOne(f => f.User)
-               .WithMany() 
-               .HasForeignKey(f => f.UserId)
-               .OnDelete(DeleteBehavior.Cascade);
-    }
+              builder.Property(f => f.Subject)
+                     .HasMaxLength(150)
+                     .IsRequired();
+
+              builder.Property(f => f.Message)
+                     .HasMaxLength(1000)
+                     .IsRequired();
+
+              builder.HasOne(f => f.User)
+                     .WithMany(u => u.Feedbacks)
+                     .HasForeignKey(f => f.UserId)
+                     .OnDelete(DeleteBehavior.Cascade);
+       }
 }
