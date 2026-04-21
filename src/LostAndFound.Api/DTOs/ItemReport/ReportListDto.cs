50c5be7b0b1f7dac75a56687c97bac6164b95b56
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-04-21 03:11:44 +0300

    feat: added ReportListDto to brows all the reports

---
+++ src/LostAndFound.Api/DTOs/ItemReport/ReportListDto.cs
@@ -0,0 +1,12 @@
+﻿using LostAndFound.Core.Enums;
+
+namespace LostAndFound.Api.DTOs.ItemReports;
+
+public class ReportListDto
+{
+    public int Id { get; set; }
+    public string ItemName { get; set; } = string.Empty;
+    public string ImagePath { get; set; } = string.Empty;
+    public DateTime DateReported { get; set; }
+    public enReportType ReportType { get; set; }
+}
