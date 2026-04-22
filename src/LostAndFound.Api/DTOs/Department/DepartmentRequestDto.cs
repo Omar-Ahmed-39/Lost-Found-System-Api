b69970a5a95dc0536dc04063d4a01da3cfe992fe
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-04-22 17:40:42 +0300

    feat: added DTOs for departemnt for input validation and output formating

---
+++ src/LostAndFound.Api/DTOs/Department/DepartmentRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LostAndFound.Api.DTOs.Departments;
+
+public class DepartmentRequestDto
+{
+    [Required]
+    [MinLength(1, ErrorMessage = "Department name cannot be empty.")]
+    [MaxLength(150, ErrorMessage = "Department name cannot exceed 150 characters.")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "A valid University is required.")]
+    public int UniversityId { get; set; }
+}
