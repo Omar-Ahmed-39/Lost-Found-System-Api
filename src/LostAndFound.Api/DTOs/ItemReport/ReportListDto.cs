032d83042a0fe6b3c4f88a3b17087195c892dd90
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-05-31 12:49:17 +0300

    feat: - make reportType string to show in the endpoint if it "lost or found" - added Status , LocationName and ReporterName.

--- src/LostAndFound.Api/DTOs/ItemReport/ReportListDto.cs
+++ src/LostAndFound.Api/DTOs/ItemReport/ReportListDto.cs
@@ -7,6 +7,9 @@ public class ReportListDto
     public int Id { get; set; }
     public string ItemName { get; set; } = string.Empty;
     public string ImagePath { get; set; } = string.Empty;
+    public string ReportType { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public string LocationName { get; set; } = string.Empty;
+    public string ReporterName { get; set; } = string.Empty;
     public DateTime DateReported { get; set; }
-    public enReportType ReportType { get; set; }
 }
