9233fcb0611b6d438513238df885ca2221b20537
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-05-31 12:50:49 +0300

    refact: Update UserReportDetailsDto image and make report type field string - added Description field to match the UI

--- src/LostAndFound.Api/DTOs/ItemReport/UserReportDetailsDto.cs
+++ src/LostAndFound.Api/DTOs/ItemReport/UserReportDetailsDto.cs
@@ -6,8 +6,9 @@ public class UserReportDetailsDto
 {
     public int Id { get; set; }
     public string ItemName { get; set; } = string.Empty;
-    public string ImagePath { get; set; } = string.Empty;
-    public enReportType ReportType { get; set; }
+    public List<ImageDto> Images { get; set; } = new();
+    public string ReportType { get; set; } = string.Empty;
     public string LocationName { get; set; } = string.Empty;
     public DateTime DateReported { get; set; }
+    public string Description { get; set; } = string.Empty;
 }
